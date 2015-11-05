5a99f08684338084a4bd90716394339f0250b9af
Thanasi Stratigakis <thanasi.strati@gmail.com>
2015-11-04 21:47:24 -0500

    Initial Commit

---
+++ TVShopperTVOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TVShopperTVOS
+//
+//  Created by Thanasi Stratigakis on 11/4/15.
+//  Copyright © 2015 TVWatchers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
